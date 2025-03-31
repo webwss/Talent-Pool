b724ca5d8fc7fa58f3e34275b69330e13a11c397
爽爽 <1306822660@qqq.com>
2025-03-31 21:10:55 +0800

    后端初始化

---
+++ Talent_Pool_Backend/src/main/java/cn/webwss/talent_pool_backend/TalentPoolBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.webwss.talent_pool_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentPoolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalentPoolBackendApplication.class, args);
+    }
+
+}
---
+++ Talent_Pool_Backend/src/test/java/cn/webwss/talent_pool_backend/TalentPoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.webwss.talent_pool_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentPoolBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
