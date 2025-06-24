411bd6c6276dbe9fee766b8f4bff88ff22e62250
爽爽 <1306822660@qq.com>
2025-06-24 09:30:33 +0800

    前端-数据看板（管理员）编写完成

---
+++ frontend/src/components/admindata/ASexTalentCount.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+
+import { Chart } from '@antv/g2';
+import {onMounted, ref} from "vue";
+import {sexTalentCountUsingPost, totalSexTalentCountUsingGet} from "@/api/dataController.ts";
+// 加载中
+const loading = ref(true);
+
+// 性别人才数
+const sexTalentCount = ref([]);
+
+
+// 获取性别人才数
+const getSexTalentCount = async () => {
+  const res = await totalSexTalentCountUsingGet();
+  if (res.data.code === 0) {
+    sexTalentCount.value = res.data.data;
+    init();
+    loading.value = false;
+  }
+}
+
+const init = () => {
+  const chart = new Chart({
+    container: 'container2',
+    autoFit: true,
+  });
+
+  chart.interval().data(sexTalentCount.value);
+  chart.axis('x', { title: "性别" });
+  chart.axis('y', { title: "人数" });
+  chart.encode('x', 'name').encode('y', 'value');
+  chart.encode('color', 'name');
+  chart.render();
+}
+
+onMounted(()=>{
+  getSexTalentCount()
+})
+
+
+
+</script>
+
+<template>
+  <h1>性别--人才数数量</h1>
+  <a-spin v-if="loading" />
+  <div id="container2"></div>
+</template>
+
+<style scoped>
+#container2 {
+  width: 100%;
+  height: 320px;
+}
+
+</style>
--- frontend/src/views/admin/AdminDataPage.vue
+++ frontend/src/views/admin/AdminDataPage.vue
@@ -1,11 +1,58 @@
 <script setup lang="ts">
 
+import {totalTalentCountUsingGet} from "@/api/dataController.ts";
+import {onMounted, ref} from "vue";
+
+const count = ref(0)
+const getCount = async () => {
+  const res = await totalTalentCountUsingGet()
+  if(res.data.code === 0) {
+    count.value = res.data.data
+  }
+}
+
+onMounted((()=>{
+  getCount()
+}))
+
+
 </script>
 
 <template>
-  <div>
-    管理员-数据看板
-  </div>
+
+  <!--  系统总人才数量-->
+  <a-row>
+    <a-col :span="24">
+      <a-statistic title="系统总人才数" :value="count" style="text-align: center;" />
+    </a-col>
+  </a-row>
+  <hr>
+
+  <a-row>
+    <a-col :span="12">
+      <!--  岗位人才数量-->
+      <APostTalentCount />
+    </a-col>
+    <a-col :span="12">
+      <!--  性别人才数量-->
+      <ASexTalentCount />
+    </a-col>
+  </a-row>
+
+  <a-row>
+    <a-col :span="12">
+      <!--  学历人才数量-->
+      <AEducationTalentCount />
+    </a-col>
+    <a-col :span="12">
+      <!--  近7日新增人才-->
+      <ANewTalentCount />
+    </a-col>
+  </a-row>
+
+
+
+
 </template>
 
 <style scoped>
